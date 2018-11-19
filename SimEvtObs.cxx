bb4f698fd4fc3c18d8d645c8ca107c251bd56cb2
yszhang95 <zysright@mail.ustc.edu.cn>
2018-11-19 14:06:41 -0600

    modify files

--- SimEvtObs.cxx
+++ SimEvtObs.cxx
@@ -1,6 +1,19 @@
 #include "SimEvtObs.h"
+#include "TMath.h"
+#include "TRandom.h"
+
 ClassImp(SimEvtGen);
 ClassImp(SimEvtObs);
+
+SimEvtGen::Generate()
+{
+    if(!mptpar) return -1;
+    TRandom3 rpt, rtheta;
+    mpt = rpt.Exp(mptpar);
+    mcosTheta = rtheta.Uniform(0.9925461516, 0.9961946981);
+    me = mpt / TMath::Sqrt(1 - mcosTheta*mcosTheta);
+    return 0;
+}
 
 SimEvtGen::~SimEvtGen()
 {
@@ -8,32 +21,30 @@ SimEvtGen::~SimEvtGen()
 
 SimEvtObs::SimEvtObs() : simevtgen(0)
 {
-    Float_t mptobs = 0;
-    Float_t meobs = 0;
-    Float_t mcosThetaobs = 0;
+    mptobs = 0;
+    meobs = 0;
+    mcosThetaobs = 0;
 
-    Float_t mpterr = 0;
-    Float_t meerr = 0;
-    Float_t mcosThetaerr = 0;
+    mpterr = 0;
+    meerr = 0;
+    mcosThetaerr = 0;
 
-    Float_t meres = 0;
-    Float_t mthetares = 0;
+    merespar =0;
 }
 
 SimEvtObs::SimEvtObs(SimEvtGen* evtgen)
 {
     simevtgen = evtgen;
 
-    Float_t mptobs = 0;
-    Float_t meobs = 0;
-    Float_t mcosThetaobs = 0;
+    mptobs = 0;
+    meobs = 0;
+    mcosThetaobs = 0;
 
-    Float_t mpterr = 0;
-    Float_t meerr = 0;
-    Float_t mcosThetaerr = 0;
+    mpterr = 0;
+    meerr = 0;
+    mcosThetaerr = 0;
 
-    Float_t meres = 0;
-    Float_t mthetares = 0;
+    merespar =0;
 }
 
 SimEvtObs::~SimEvtObs()
@@ -41,14 +52,88 @@ SimEvtObs::~SimEvtObs()
     if(simevtgen) delete simevtgen;
 }
 
-Int_t SimEvtObs::SetERes(Float_t res)
+Int_t SimEvtObs::SetEResParAndErr(Float_t par)
 {
-    if (simevtgen) {meres = res; return 0;}
+    if (simevtgen) {
+        if(!par) return -1;
+        merespar = par; 
+        meerr = TMath::Sqrt(simevtgen->GetE()) * merespar;
+        return 0;
+    }
     if (!simevtgen) { return -1; }
 }
 
-Int_t SimEvtObs::SetThetaRes(Float_t res)
+Int_t SimEvtObs::SetThetaErr(Float_t err)
 {
-    if (simevtgen) {mthetares = res; return 0;}
+    if (simevtgen) {mthetaerr = err; return 0;}
     if (!simevtgen) { return -1; }
+}
+
+Int_t SimEvtObs::SetCosTheta(Float_t costheta)
+{
+    if (simevtgen) {
+        mcosThetaobs = costheta;
+        return 0;
+    }
+    if (!simevtgen) {
+        return -1;
+    }
+}
+
+Int_t SetE()
+{
+    if (simevtgen) {
+        TRandom3 r;
+        if (!meerr) return -1;
+        meobs = r.Gaus(simevtgen->GetE(), meerr);
+        return 0
+    }
+    return -1;
+}
+
+Int_t SimEvtObs::SetE(Float_t e)
+{
+    if (simevtgen) {
+        meobs = e;
+        return 0;
+    }
+    if (!simevtgen) {
+        return -1;
+    }
+
+Int_t SimEvtObs::SetPt()
+{
+    if (simevtgen){
+        if(!meobs) return -1;
+        mptobs = meobs * TMath::Sqrt(1 - mcosThetaobs * mcosThetaobs);
+        return 0;
+    }
+    if (!simevtgen){
+        return -1;
+    }
+}
+
+Int_t SimEvtObs::SetPt(Float_t pt)
+{
+    if (simevtgen) {
+        mptobs = pt;
+        return 0;
+    }
+    if (!simevtgen) {
+        return -1;
+    }
+}
+Int_t SimEvtObs::SetPtErr()
+{
+    if (simevtgen) {
+        Float_t sumw2 = 0;
+        Float_t sinTheta = TMath::Sqrt(1 - mcosThetaobs *mcosThetaobs);
+        Float_t err1 = mcosThetaobs * meobs / sinTheta; 
+        sumw2 += TMath::Power(err1, 2);
+        Float_t err2 = sinTheta * meerr;
+        sumw2 += TMath::Power(err2, 2);
+        mpterr = TMath::Sqrt(sumw2);
+        return 0;
+    }
+    return -1
 }
--- SimEvtObs.h
+++ SimEvtObs.h
@@ -9,10 +9,12 @@ class SimEvtGen : public TObject {
 public:
     SimEvtGen() {}; 
     virtual ~SimEvtGen();
-    Int_t SetPt(Float_t pt) { mpt = pt; return 0; };
-    Int_t SetE(Float_t e) { me = e; return 0; };
-    Int_t SetCosTheta(Float_t cosTheta) { mcosTheta = cosTheta; return 0; };
-    void ClearInfo() { mpt=0; me=0; mcosTheta=0; };
+    // void SetPt(Float_t pt) { mpt = pt; };
+    // void SetE(Float_t e) { me = e; };
+    // void SetCosTheta(Float_t cosTheta) { mcosTheta = cosTheta; };
+    void SetPtPar(Float_t par) { mptpar = par; };
+    void ClearInfo() { mptpar=0; mpt=0; me=0; mcosTheta=0; };
+    Int_t Generate();
     Float_t GetPt() const {return mpt; } ;
     Float_t GetE() const {return me; } ;
     Float_t GetCosTheta() const {return mcosTheta; } ;
@@ -20,6 +22,7 @@ public:
 private:
     SimEvtGen& operator=(const SimEvtGen&);
     SimEvtGen(const SimEvtGen&);
+    Float_t mptpar;
     Float_t mpt;
     Float_t me;
     Float_t mcosTheta;
@@ -32,8 +35,14 @@ class SimEvtObs : public TObject{
     SimEvtObs();
     SimEvtObs(SimEvtGen*);
     virtual ~SimEvtObs();
-    Int_t SetERes(Float_t);
-    Int_t SetThetaRes(Float_t);
+    Int_t SetEResAndPar(Float_t);
+    Int_t SetThetaErr(Float_t);
+    Int_t SetPtErr();
+    Int_t SetCosTheta(Float_t);
+    Int_t SetE();
+    Int_t SetE(Float_t);
+    Int_t SetPt();
+    Int_t SetPt(Float_t);
 
   private:
     SimEvtObs& operator=(const SimEvtGen&);
@@ -50,8 +59,7 @@ class SimEvtObs : public TObject{
     Float_t meerr;
     Float_t mcosThetaerr;
     // resolutions for detector
-    Float_t meres;
-    Float_t mthetares;
+    Float_t merespar;
 
     ClassDef(SimEvtObs, 1)
 };
