d082b6e522f18ee7de8d9ab99a17eda10a54c665
yszhang95 <zysright@mail.ustc.edu.cn>
2018-11-19 13:01:43 +0800

    modify the headers

--- SimEvtGen.h
+++ SimEvtGen.h
@@ -4,15 +4,14 @@
 class SimEvtGen : public TObject {
 public:
     SimEvtGen(); 
-    SimEvtGen(Float_t pt, Float_t cosTheta);
     virtual ~SimEvtGen();
-    Int_t SetPt(Float_t pt) {mpt = pt; return 0};
-    Int_t SetE(Float_t e) {me = e; return 0};
-    Int_t SetCosTheta(Float_t cosTheta) {mcosTheta = cosTheta; return 0};
-    Int_t Clear() {mpt=0; e=0; cosTheta=0};
-    const Float_t GetPt(return pt) const;
-    const Float_t GetE(return e) const;
-    const Float_t GetCosTheta(return cosTheta) const;
+    Int_t SetPt(Float_t pt) { mpt = pt; return 0; };
+    Int_t SetE(Float_t e) { me = e; return 0; };
+    Int_t SetCosTheta(Float_t cosTheta) { mcosTheta = cosTheta; return 0; };
+    Int_t Clear() { mpt=0; me=0; mcosTheta=0; };
+    const Float_t GetPt(){return mpt; } const;
+    const Float_t GetE(){return me; } const;
+    const Float_t GetCosTheta(){return mcosTheta; } const;
 
 private:
     SimEvtGen& operator=(const SimEvtGen&);
@@ -22,5 +21,5 @@ private:
     Float_t mcosTheta;
 
     ClassDef(SimEvtGen, 1);
-}
+};
 #endif
--- SimEvtObs.h
+++ SimEvtObs.h
@@ -1,3 +1,35 @@
 #ifndef SimEvtObs_h
 #define SimEvtObs_h
+
+class SimEvtGen;
+class TNtuple;
+
+class SimEvtObs : public TObject{
+  public:
+    SimEvtObs();
+    SimEvtObs(SimEvtGen*);
+    SetERes(Float_t );
+    ~virtual SimEvtObs();
+
+  private:
+    SimEvtObs& operator=(const SimEvtGen&);
+    SimEvtObs(const SimEvtGen&);    
+
+    // variables of true event
+    Float_t mptgen;
+    Float_t megen;
+    Float_t mcosThetagen;
+    // variables of observed event
+    Float_t mptobs;
+    Float_t meobs;
+    Float_t mcosThetaobs;
+    // errors of observed variables
+    Float_t mpterr;
+    Float_t meerr;
+    Float_t mcosThetaerr;
+    // tree for storage
+    TNtuple* nt;
+
+    ClassDef(SimEvtObs, 1)
+};
 #endif
