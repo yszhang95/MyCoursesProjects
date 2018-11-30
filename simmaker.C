e3dffcf73d7ff6e5a1b3e695b2c5c30a0932697f
yszhang95 <zysright@mail.ustc.edu.cn>
2018-11-30 15:34:22 -0600

    modify files

--- simmaker.C
+++
@@ -1,9 +0,0 @@
-#include "SimMaker.h"
-R__LOAD_LIBRARY(SimMaker_cxx)
-void simmaker()
-{
-    SimMaker *maker = new SimMaker();
-    maker->Init("problemA.root");
-    maker->Make(1000000);
-    maker->Finish();
-}
